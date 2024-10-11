3bc61d127bdc2ecbf6fbc8fb5c0946e044d9b89e
otega07 <okiriotega@gmail.com>
2024-10-11 17:48:49 +0100

    Modify core/views.py to improve functionality

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 # Create your views here.
+def home(request):
+    return HttpResponse("Welcome to SMOT Socials Homepage")
 
 def test_api(request):
     return JsonResponse({"message": "Hello from Django!"})
