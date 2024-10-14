907fd0b2ccaa5ec2bb464533ad4d244d5fb19d5e
Teedex1 <opeyemiridwan8965@gmail.com>
2024-10-14 17:19:34 +0100

    Stop tracking node_modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-cd import React from 'react'; 
+import React from 'react'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Home from './pages/Home';
 import About from './pages/About';
