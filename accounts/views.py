1ebd2fbc4012075750c0bff5639d4643f3d8d947
otega07 <okiriotega@gmail.com>
2024-10-16 17:49:49 +0100

    forms for user registration/login and views for authentication and profile management

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import LoginForm, RegistrationForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after login
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/index.html', {'form': form, 'form_type': 'login'})
+
+def user_register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the home page after registration
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/index.html', {'form': form, 'form_type': 'register'})
