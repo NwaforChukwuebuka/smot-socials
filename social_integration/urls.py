e78f839b8bedcbdd3179546fdea28cca67b812cc
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-10-24 20:07:51 +0100

    feat: Added urls api routes

--- social_integration/urls.py
+++ social_integration/urls.py
@@ -1,13 +1,25 @@
-# smot/social_integration/urls.py
+from django.urls import path, include
+from social_django.urls import urlpatterns as social_auth_urls
+from .views import (
+    unlink_social_account,
+    linkedin_complete, 
+    linkedin_post,
+    post_to_twitter_view,
+)
+from . import views
 
-from django.urls import path
-from .views import UserConnectionsView,post_to_twitter
-# from .views import switch_account
 
 urlpatterns = [
-     path('api/user/connections/', UserConnectionsView.as_view(), name='user-connections'),
-     path('api/twitter/post/', post_to_twitter, name='post_to_twitter'),
-    # path('switch_account/<str:provider>/', switch_account, name='switch_account'),
-    # # Other URL patterns can be added here...
+    path('linkedin/post/', linkedin_post, name='linkedin_post'),
+  # LinkedIn post URL
+    path('unlink/<str:platform>/', unlink_social_account, name='unlink_social_account'),
+    path('social/auth/complete/linkedin-oauth2/', linkedin_complete, name='linkedin_complete'),  # LinkedIn OAuth complete URL
+    path('twitter/connect/', views.twitter_connect, name='twitter_connect'),
+    path('twitter/callback/', views.twitter_callback, name='twitter_callback'),
+    path('post_to_twitter/', post_to_twitter_view, name='post_to_twitter_view'),
 ]
+
+
+# Include the social-auth URLs for handling OAuth logins and completions (e.g., for LinkedIn, Facebook)
+urlpatterns += social_auth_urls
 
