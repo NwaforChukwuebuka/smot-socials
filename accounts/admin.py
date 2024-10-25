91eccf3def4acb7778d6ef6d0fbb3065908d8c61
otega07 <okiriotega@gmail.com>
2024-10-25 02:04:03 +0100

    fix(admin): Customize UserAdmin list_display with user ID and re-register User model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User  # Or your custom user model
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')  # Add 'id' here
+
+admin.site.unregister(User)  # Unregister the default User admin
+admin.site.register(User, UserAdmin)  # Register it again with the custom admin class
