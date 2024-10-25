65136d74711f170b3aa639fd3e373c48af04c1a2
otega07 <okiriotega@gmail.com>
2024-10-25 01:48:00 +0100

    fix: removed merged conflict files

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # Specify the fields to be displayed in the admin interface
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('email',)
+    search_fields = ('email', 'first_name', 'last_name')
+    filter_horizontal = ()
+    list_select_related = ()
+    
+    # Define the form for creating and updating users
+    fieldsets = (
+        (None, {'fields': ('email', 'first_name', 'last_name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+
+    # Override the save method to handle password hashing
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.set_password(form.cleaned_data['password'])
+        obj.save()
+
+admin.site.register(User, CustomUserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Model for storing site-wide settings and configurations
 class SiteSettings(models.Model):
@@ -89,3 +91,44 @@ class UserFeedback(models.Model):
     class Meta:
         verbose_name = "User Feedback"
         verbose_name_plural = "User Feedbacks"
+        
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.CharField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def get_group_permissions(self, obj=None):
+        return []
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
